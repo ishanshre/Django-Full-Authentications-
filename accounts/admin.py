1211c97f337de8b713b7ceebdbd9d9b526bb5a83
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-10-05 13:52:25 +0545

    Register custom user model to admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from django.contrib.auth import get_user_model
 # Register your models here.
+
+@admin.register(get_user_model())
+class UserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = ['username','email','is_staff','is_active']
+    fieldsets = UserAdmin.fieldsets + (('Additional Information', {'fields':('date_of_birth','gender',)}),)
+    add_fieldsets = UserAdmin.fieldsets + (('Additional Information', {'fields':('date_of_birth', 'gender',)}),)
